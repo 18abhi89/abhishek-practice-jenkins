043b41384d40799d6afd27d4aefb4efdc1013582
Abhishek Saxena <18abhi89@gmail.com>
2023-10-16 17:26:07 +0530

    added sout

--- src/main/java/com/abhi/practicea/PracticeaApplication.java
+++ src/main/java/com/abhi/practicea/PracticeaApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticeaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PracticeaApplication.class, args);
+
+		System.out.println("hi how are you?");
 	}
 
 }
